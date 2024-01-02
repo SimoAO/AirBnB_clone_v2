394422496695810ee7fffa29861b9b5dc04fb022
simoao <aitouaazizmohammed@gmail.com>
2024-01-02 02:15:59 +0100

    task12

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Starting Flask web application module """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	""" HBnB filters class """
+	states = storage.all(State)
+	amenities = storage.all(Amenity)
+    places = storage.all(Place)
+	return render_template("100-hbnb.html",
+            states=states, amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ Remove SQLAlchemy class """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
